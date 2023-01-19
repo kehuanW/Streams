146852d7205de9bd2b7d21656f8bd4ebb5484add
Kehuan <kehuanwang123@gmail.com>
2023-01-19 21:43:15 +1030

    good locally

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
         <div className='ui secondary pointing menu'>
             <Link className='item' to="/">Streamer</Link>
             <div className='right menu'>
-                <Link className='item' to="/">All Streams</Link>
+                <Link className='item' to="/" style={{ fontWeight: "bold" }}>All STREAMS</Link>
             </div>
             <GoogleAuth />
         </div>
